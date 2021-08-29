62fd7ca2a67e967e72831cc2a7bb9859fbed5ae2
Taimur <taimur.shabbir@gmail.com>
2021-08-29 19:29:18 +0500

    FCC07 AI powered TicTacToe by Kylie Ying completed!!

--- FCC07_AI_TicTacToe/gameAI.py
+++ FCC07_AI_TicTacToe/gameAI.py
@@ -1,5 +1,5 @@
 import time
-from player import HumanPlayer, RandomComputerPlayer
+from playerAI import HumanPlayer, RandomComputerPlayer, GeniusComputerPlayer
 
 
 class TicTacToe:
@@ -108,14 +108,28 @@ def play(game, x_player, o_player, print_game=True):
             #     letter = 'X'
 
         # tiny break
-        time.sleep(0.8)
+        if print_game:
+            time.sleep(0.8)
 
     if print_game:
         print('It\'s a tie!')
 
 
 if __name__ == '__main__':
-    x_player = HumanPlayer('X')
-    o_player = RandomComputerPlayer('O')
-    t = TicTacToe()
-    play(t, x_player, o_player, print_game=True)
+    x_wins = 0
+    o_wins = 0
+    ties = 0
+    for _ in range(10):
+        x_player = RandomComputerPlayer('X')
+        o_player = GeniusComputerPlayer('O')
+        t = TicTacToe()
+        result = play(t, x_player, o_player, print_game=False)
+        if result == 'X':
+            x_wins += 1
+        elif result == 'O':
+            o_wins += 1
+        else:
+            ties += 1
+
+    print(
+        f'After 1000 iterations, we see {x_wins} X wins, {o_wins} O wins, and {ties} ties')
